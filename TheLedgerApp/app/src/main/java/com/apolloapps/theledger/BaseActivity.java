8d33b1b44232b8a75d97c1e33a42c78c7794ff1f
Alfredo Moreira <Alfredo.Moreira@davita.com>
2016-04-04 18:18:44 -0600

    Base set up

--- TheLedgerApp/app/src/main/java/com/apolloapps/theledger/BaseActivity.java
+++ TheLedgerApp/app/src/main/java/com/apolloapps/theledger/BaseActivity.java
@@ -1,12 +1,18 @@
 package com.apolloapps.theledger;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by AMoreira on 4/4/16.
  */
 public class BaseActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,30 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        mToolbar =(Toolbar) findViewById(R.id.toolbar);
+        if(mToolbar != null) {
+            setUpToolBar();
+        } else {
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    BaseActivity.this.onBackPressed();
+                }
+            });
+        }
+
+    }
+
+    private void setUpToolBar() {
+        final Drawable upArrow = ResourcesCompat.getDrawable(getResources(), R.drawable.abc_ic_ab_back_mtrl_am_alpha, null);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+    }
+
+    public void setActionBarTitle(String title) {
+        mToolbar.setTitle(title);
     }
 
     @Override
@@ -33,9 +63,17 @@ public class BaseActivity extends AppCompatActivity {
         super.onStop();
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    public Toolbar getToolbar() {
+        return mToolbar;
     }
 }
